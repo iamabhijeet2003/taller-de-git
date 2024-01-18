9c7e19ab426a1df9f5642b3aad5be1825b86feae
abhijeet <abhijeet3016@gmail.com>
2024-01-18 19:53:03 +0100

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,6 +1,6 @@
 <?php
-// Autor - Abhijeet Singh <abhijeet3016@gmail.com>
-// El nombre por defecto es Mundo
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
-@print "Hola {$nombre}\n";
+// Autor: Abhijeet Singh  <abhijeet3016@gmail.com>
+print "Introduce tu nombre:";
+$nombre = trim(fgets(STDIN));
+@print "Hola, {$nombre}\n";
 ?>
